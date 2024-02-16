d3ecaba7ec917da95c8f5eb1488f24cc0eeb761e
Oğuzhan Delibaş <64430254+oguzhandelibas@users.noreply.github.com>
2024-02-16 13:45:49 +0300

    +refactor: techstack & toolstack

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -3,15 +3,26 @@ import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 
 import {
-    SiCsharp
+    SiCsharp,
+    SiSwift,
+    SiPython
 } from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
         <Col xs={4} md={2} className="tech-icons">
-        <SiCsharp />
-      </Col>
+            <SiCsharp />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <CgCPlusPlus />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiPython />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiSwift />
+        </Col>
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
+  SiVisualstudio,
+    SiRider,
+  SiMacos, 
+    SiUnrealengine,SiTrello, SiFigma, SiGithub, SiXcode, SiNotion, SiAdobeillustrator, SiAdobepremierepro
 } from "react-icons/si";
-import {DiUnitySmall} from "react-icons/di";
+import {
+    DiUnitySmall,
+    DiIllustrator, DiVisualstudio,
+
+} from "react-icons/di";
 
 function Toolstack() {
   return (
@@ -15,6 +18,34 @@ function Toolstack() {
         <Col xs={4} md={2} className="tech-icons">
             <DiUnitySmall />
         </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiUnrealengine />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiVisualstudio />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiTrello />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiFigma />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiGithub />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiXcode />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiNotion />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiAdobeillustrator />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiAdobepremierepro />
+        </Col>
+        
     </Row>
   );
 }
